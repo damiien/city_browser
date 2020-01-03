b427c15d2a7711060f56fb0c3c98334dcef14d78
damiien <dame.insct@gmail.com>
2020-01-03 15:07:50 +0100

    Create Home.jsx

---
+++ src/static/js/components/Home.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import CityBrowser from "./CityBrowser";
+
+export default class Home extends Component {
+    render() {
+       return (
+          <div>
+			  <CityBrowser />
+		  </div>
+       )
+    }
+}
